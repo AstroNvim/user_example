435e0cd5bcb845a1a59a7c5336c32726d115eaad
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-03-02 22:10:45 +0530

    fix(options): update diagnostic options

--- options.lua
+++ options.lua
@@ -13,9 +13,17 @@ return {
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true, -- enable completion at start
     autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_enabled = true, -- enable diagnostics at start
-    status_diagnostics_enabled = true, -- enable diagnostics in statusline
+    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
+-- If you need more control, you can use the function()...end notation
+-- return function(local_vim)
+--   local_vim.opt.relativenumber = true
+--   local_vim.g.mapleader = " "
+--   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+--   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+--
+--   return local_vim
+-- end

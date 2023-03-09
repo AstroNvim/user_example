173d7f67a003d0d853174a5104ab3bd275b47070
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-03-09 18:26:34 +0530

    fix(mason): sumneko_lua -> lua_ls
    Signed-off-by: Utkarsh Gupta <utkarshgupta137@gmail.com>

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "sumneko_lua" },
+      -- ensure_installed = { "lua_ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources

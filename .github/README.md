2d62b10020e45a007efc08357065031eae1e5f2c
Micah Halter <micah@mehalter.com>
2023-03-10 10:41:34 -0500

    chore: use stable astronvim

--- .github/README.md
+++ .github/README.md
@@ -2,8 +2,6 @@
 
 A user configuration template for [AstroNvim](https://github.com/AstroNvim/AstroNvim)
 
-**Note:** This repository does require AstroNvim v3.0. Because of this, `nightly` is currently the default update channel until v3.0 is released to stable when it will be changed to using `stable` by default.
-
 ## 🛠️ Installation
 
 #### Make a backup of your current nvim and shared folder
--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ return {
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
-    channel = "nightly", -- "stable" or "nightly"
+    channel = "stable", -- "stable" or "nightly"
     version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
     branch = "nightly", -- branch name (NIGHTLY ONLY)
     commit = nil, -- commit hash (NIGHTLY ONLY)

821aa73190976814d57b63171d9288c58575fdd8
Micah Halter <micah@mehalter.com>
2023-03-11 20:52:24 -0500

    fix(plugins): fix default configuration syntax

--- plugins/core.lua
+++ plugins/core.lua
@@ -27,7 +27,7 @@ return {
   -- {
   --   "L3MON4D3/LuaSnip",
   --   config = function(plugin, opts)
-  --     plugin.default_config(opts) -- include the default astronvim config that calls the setup call
+  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
   --     -- add more custom luasnip configuration such as filetype extend or custom snippets
   --     local luasnip = require "luasnip"
   --     luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -36,7 +36,7 @@ return {
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
-  --     plugin.default_config(opts) -- include the default astronvim config that calls the setup call
+  --     require "plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
   --     -- add more custom autopairs configuration such as custom rules
   --     local npairs = require "nvim-autopairs"
   --     local Rule = require "nvim-autopairs.rule"
@@ -66,7 +66,7 @@ return {
   -- {
   --   "folke/which-key.nvim",
   --   config = function(plugin, opts)
-  --     plugin.default_config(opts)
+  --     require "plugins.configs.which-key"(plugin, opts) -- include the default astronvim config that calls the setup call
   --     -- Add bindings which show up as group name
   --     local wk = require "which-key"
   --     wk.register({

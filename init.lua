14ca83640abce7ac34d1d836bdc747b17337e801
Micah Halter <micah@mehalter.com>
2023-03-17 11:55:51 -0400

    chore: update disable plugins

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ return {
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
+        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
       },
     },
   },

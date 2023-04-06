aa02698caa1054daae1ed723806d56b9aef9d6f5
Logan Luo <logan.luo@cronoslabs.com>
2023-04-06 13:25:22 +0800

    docs: update lua lsp formatting comments

--- init.lua
+++ init.lua
@@ -40,7 +40,8 @@ return {
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function

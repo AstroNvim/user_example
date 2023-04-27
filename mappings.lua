a579c245fde1c21e9e75b38e93ef255c217edd42
Adrian Filip <realadrianfilip@gmail.com>
2023-04-28 01:13:36 +0300

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -24,6 +24,20 @@ return {
     ["<S-Tab>"] = {":bprevious<CR>", desc = "Previous buffer" },
     ["gD"] = {vim.lsp.buf.definition, desc = "Go to definition" },
     ["gi"] = {vim.lsp.buf.implementation, desc = "Go to implementation" },
+    ["K"] =  {vim.lsp.buf.hover, desc = "Hover" },
+    ["gr"] = {vim.lsp.buf.references, desc = "References" },
+    ["gds"] = {vim.lsp.buf.document_symbol, desc = "Document symbol" },
+    ["gws"] = {vim.lsp.buf.workspace_symbol, desc = "Workspace symbol" },
+    ["<leader>sh"] = {vim.lsp.buf.signature_help, desc = "Signature help" },
+    ["<leader>rn"] = {vim.lsp.buf.rename, desc = "Rename" },
+    ["<leader>ka"] = {vim.lsp.buf.code_action, desc = "K(C)ode action"},
+    ["<leader>aa"] = {vim.diagnostic.setqflist, desc = "All workspace diagnostics"},
+    ["<leader>ae"] = {function() vim.diagnostic.setqflist({ severity = "E" }) end, desc = "All workspace errors"},
+    ["<leader>aw"] = {function() vim.diagnostic.setqflist({ severity = "W" }) end, desc = "All workspace warnings"},
+    ["<leader>d"] = {vim.diagnostic.setloclist, desc = "Buffer diagnostics" },
+    ["[c"] = {function() vim.diagnostic.goto_prev({ wrap = false }) end, desc = "Previous diagnostic" },
+    ["]c"] = {function() vim.diagnostic.goto_next({ wrap = false }) end, desc = "Next diagnostic" },
+    
   },
   t = {
     -- setting a mapping to false will disable it

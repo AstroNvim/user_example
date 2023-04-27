4bf929e96d5b845b549a2dcdff5127d4599703b5
Adrian Filip <realadrianfilip@gmail.com>
2023-04-27 23:11:04 +0300

    Update options.lua

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+  },
+  opts = {
     background_colour = "#000000",
   },
   g = {

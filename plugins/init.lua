3db536edbde6e6c30ee73e418ba9b7ccdb1852de
Adrian Filip <realadrianfilip@gmail.com>
2023-04-28 00:14:27 +0300

    Update init.lua

--- plugins/init.lua
+++ plugins/init.lua
@@ -6,4 +6,11 @@ return {
       require("catppuccin").setup {}
     end,
   },
+  {
+    "folke/tokyonight.nvim",
+    as = "tokyonight",
+    config = function()
+      require("tokyonight").setup {}
+    end,
+  },
 }

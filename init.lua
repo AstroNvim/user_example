22deffeb2a6807a4ac39053bfdbe5ef3ea9cbeba
Adrian Filip <realadrianfilip@gmail.com>
2023-04-27 22:52:32 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,7 +18,18 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  --colorscheme = "astrodark",
+  colorscheme = "catppuccin",
+  
+  plugins = {
+    {
+      "catppuccin/nvim",
+      as = "catppuccin",
+      config = function()
+        require("catppuccin").setup {}
+      end,
+    },
+  },
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {

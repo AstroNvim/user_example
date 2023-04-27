90093379a5bb7154dea0ca030c886d0e1aaa2f89
Adrian Filip <realadrianfilip@gmail.com>
2023-04-27 19:41:38 +0300

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -20,8 +20,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    { "<Tab>", ":bnext<CR>", { noremap = true, silent = true } },
-    { "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true } },
+    -- { "<Tab>", ":bnext<CR>", { noremap = true, silent = true } },
+    -- { "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true } },
   },
   t = {
     -- setting a mapping to false will disable it

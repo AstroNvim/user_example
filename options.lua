381df539676eea9e5d872829d6de94d31eec8533
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-07-12 10:10:25 +0600

    feat(options): add option for control over telescope lsp integration

--- options.lua
+++ options.lua
@@ -17,6 +17,7 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    telescope_lsp_integration_enabled = true, -- enable telescope integration for lsp keymaps
   },
 }
 -- If you need more control, you can use the function()...end notation

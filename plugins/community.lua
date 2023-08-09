cffede3afe609f771ce3c06bb566042166e30fdd
TomAintAround <tcorreia2005@gmail.com>
2023-08-09 22:27:50 +0100

    fix(community): fix spell mistake

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
+  -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },

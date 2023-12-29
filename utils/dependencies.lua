081d5cf50d70af993897375cf33b3afcc433fa65
manuuurino <2855338+manuuurino@users.noreply.github.com>
2023-12-29 23:41:10 +0100

    fix(utils): has_executable was inversed

--- utils/dependencies.lua
+++ utils/dependencies.lua
@@ -2,7 +2,7 @@
 ---@return boolean
 local function has_excecutable(executable)
 	-- TODO: warn the user if not found
-	return vim.fn.executable(executable) == 0
+	return vim.fn.executable(executable) == 1
 end
 
 ---@return boolean

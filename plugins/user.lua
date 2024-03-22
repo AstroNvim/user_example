3a440a28b5be88b48f54a51e3db0dea825007c9c
ekusiadadus <ekusiadadus@gmail.com>
2024-03-22 11:00:23 +0900

    feat: update nvim settings

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,22 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "TimUntersberger/neogit",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
+  },
+  {
+    "wakatime/vim-wakatime",
+  }
 }

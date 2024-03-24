0394902d5fcf8f75acae59580a279b05e487bed8
ekusiadadus <ekusiadadus@gmail.com>
2024-03-25 08:19:52 +0900

    feat: add copilot settings

--- plugins/user.lua
+++ plugins/user.lua
@@ -37,6 +37,75 @@ return {
   },
   {
     "github/copilot.vim",
-    lazy=false,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function() vim.g.copilot_no_tab_map = true end,
+  },
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    opts = function(_, opts)
+      local cmp, copilot = require "cmp", require "copilot.suggestion"
+      local snip_status_ok, luasnip = pcall(require, "luasnip")
+      if not snip_status_ok then return end
+      local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+      if not opts.mapping then opts.mapping = {} end
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if copilot.is_visible() then
+          copilot.accept()
+        elseif cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<C-x>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.next() end
+      end)
+
+      opts.mapping["<C-z>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.prev() end
+      end)
+
+      opts.mapping["<C-right>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.accept_word() end
+      end)
+
+      opts.mapping["<C-l>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.accept_word() end
+      end)
+
+      opts.mapping["<C-down>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.accept_line() end
+      end)
+
+      opts.mapping["<C-j>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.accept_line() end
+      end)
+
+      opts.mapping["<C-c>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.dismiss() end
+      end)
+
+      return opts
+    end,
   },
 }

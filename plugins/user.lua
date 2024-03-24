4d79f1e7dccb658cfa990ffd865290e89080cd18
ekusiadadus <ekusiadadus@gmail.com>
2024-03-25 08:01:40 +0900

    feat: add neogit, copilot

--- plugins/user.lua
+++ plugins/user.lua
@@ -19,5 +19,24 @@ return {
   },
   {
     "wakatime/vim-wakatime",
-  }
+  },
+  {
+    "TimUntersberger/neogit",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
+  },
+  {
+    "github/copilot.vim",
+    lazy=false,
+  },
 }
